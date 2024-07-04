13777cffa998b8b61463c7c07285ec32a66b5b72
bressar <dougbressar@gmail.com>
2024-07-04 12:53:20 +0100

    update_04_07_24

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #Projeto básico de Flask - 'Jogoteca'  na versão 2 implanto o banco de dados...
-
 from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 class Jogo:
@@ -8,11 +7,27 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
-
 jogo1 = Jogo("Tetris", "Puzzle", "Atari")
 jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
 jogo3 = Jogo("Mortal Combat", "Luta", "PS2")
 lista = [jogo1, jogo2, jogo3] # lista global
+
+class Usuario:
+    def __init__(self, nome, nickname, senha):
+        self.nome = nome
+        self.nickname = nickname
+        self.senha = senha
+
+usuario1 = Usuario("John Lennon", "JL", "pass1")
+usuario2 = Usuario("Paul Macartney", "PM", "pass2")
+usuario3 = Usuario("George Harrison", "GH", "pass3")
+usuario4 = Usuario("Ringo Star", "RS", "pass4")
+
+usuarios = {usuario1.nickname : usuario1,
+            usuario2.nickname : usuario2,
+            usuario3.nickname : usuario3,
+            usuario4.nickname : usuario4 }
+
 
 app = Flask(__name__) # cria uma aplicação/objeto da classe Flask
 app.secret_key = 'bressar' # Depois de instanciar a classe Flask,
@@ -30,6 +45,7 @@ def novo():
     if 'usuario_logado' not in session or session['usuario_logado'] == None: # se não tiver usuario em session direciona para a pagina de login
         return redirect(url_for('login', proxima=url_for('novo'))) # query string: ?proxima=novo
     return render_template('novo.html', titulo="Novo Jogo")
+
 
 @app.route('/criar', methods=['POST',]) # precisa determinar o método na rota
 def criar():
@@ -50,8 +66,19 @@ def login():
 
 @app.route('/autenticar', methods=['POST',])
 def autenticar():
-    if 'alohomora' == request.form['senha']:
-        session['usuario_logado'] = request.form['usuario']
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' logado com sucesso!')  # mostra uma mensagem rápida para o user
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
+    else:
+        flash('Usuário não logado!')
+        return redirect(url_for('login'))
+
+    # if 'alohomora' == request.form['senha']:
+    #     session['usuario_logado'] = request.form['usuario']
         # Session é um recurso do Flask que permite um armazenamento temporário de dados que persiste as informações
         # coletadas por mais de um ciclo de request.
         # A session consegue persistir os dados através de cryptographically-signed cookies
@@ -66,12 +93,12 @@ def autenticar():
         # Portanto, apesar de não ser recomendada a utilização de sessions para armazenamento
         # de dados sensíveis e dependerem do tamanho máximo de armazenamento dos cookies (por volta de 4 KB),
         # sessions se apresentam como um recurso poderoso do Flask, sendo rápidas de instanciar e fáceis de escalar..
-        flash(session['usuario_logado'] + ' logado com sucesso!')# mostra uma mensagem rápida para o user
-        proxima_pagina = request.form['proxima']
-        return redirect(proxima_pagina)
-    else:
-        flash('Usuário não logado!')
-        return redirect(url_for('login'))
+        # flash(session['usuario_logado'] + ' logado com sucesso!')# mostra uma mensagem rápida para o user
+        # proxima_pagina = request.form['proxima']
+        # return redirect(proxima_pagina)
+    # else:
+    #     flash('Usuário não logado!')
+    #     return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
@@ -84,10 +111,9 @@ if __name__ == '__main__':
 
 # app.run(debug=True) # Dessa maneira sem epecificar a porta não rodava no meu pc com windows...
 
-#app.run() # http://127.0.0.1:5000 + '/inicio' "@app.route('/inicio')"pip
+#app.run() # http://127.0.0.1:5000 + '/inicio' "@app.route('/inicio')"
 # roda a aplicação na porta 5000 e o endereço como 127.0.0.1.
 
 # app.run(host='0.0.0.0', port=8080)
 # Se quiser usar a porta 8080 para aplicação ou até mesmo
 # permitir acessos externos à aplicação definindo o host como 0.0.0.0,
-
